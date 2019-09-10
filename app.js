5d286236df44af1429e085802692a1783fd7c654
jim conallen <jconallen@us.ibm.com>
2019-09-10 12:05:34 -0400

    added config map

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("<h1>Yo World from Appsody!</h1>");
+  res.send("<h1>Yo World from Appsody!  with config maps</h1>");
 });
  
 module.exports.app = app;
